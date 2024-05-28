a4bafefeda6b7babb98e5919c5f6574fdeefcd04
zinzin68 <dnwls3958@naver.com>
2024-05-28 13:31:58 +0900

    start

--- hello.java
+++ hello.java
@@ -1,5 +1,20 @@
+import java.io.*;
+import java.text.NumberFormat;
+
 public class hello {
     public static void main(String[] args) {
-        System.out.println("hello");
+        try{
+            File file = new File("C:\\Users\\dnwls\\OneDrive\\바탕 화면\\4-1\\javaprogramming\\project\\wordlist.txt");
+            FileReader filereader = new FileReader(file);
+            int singleCh =0;
+            while ((singleCh = filereader.read()) != -1){
+                System.out.print((char)singleCh);
+            }
+            filereader.close();
+        }catch (FileNotFoundException e){
+
+        }catch (IOException e){
+            System.out.println(e);
+        }
     }
 }
